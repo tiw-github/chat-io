ca9c91b1135ea12080bc4b85a7e8ef8761e50c12
wisasoft <wisasoft001@gmail.com>
2021-06-30 13:47:53 +0700

    tiw

--- index.js
+++ index.js
@@ -6,34 +6,66 @@ var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// Chatroom
 
-console.log("outside io");
+let numUsers = 0;
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
+  let addedUser = false;
 
-  console.log('User Conncetion');
-
-  socket.on('connect user', function(user){
-    console.log("Connected user ");
-    io.emit('connect user', user);
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
   });
 
-  socket.on('on typing', function(typing){
-    console.log("Typing.... ");
-    io.emit('on typing', typing);
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
   });
 
-  socket.on('chat message', function(msg){
-    console.log("Message " + msg['message']);
-    io.emit('chat message', msg);
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
   });
-});
 
-http.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
